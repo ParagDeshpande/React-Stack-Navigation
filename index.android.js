124dfa3d2b1e61d87fd040104b33c2b0dab61539
Patrick Miller <parag.deshpande1@gmail.com>
2017-07-24 21:23:09 +1200

    Second

--- index.android.js
+++ index.android.js
@@ -1,53 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from "react";
+import { AppRegistry } from "react-native";
+import App from "./src/App";
+import { StackNavigator } from "react-navigation";
+import SecondScreen from "./src/SecondScreen";
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const reactNavigationSample = props => {
+  return <App navigation={props.navigation} />;
+};
 
-export default class React_Stack_Navigation extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+reactNavigationSample.navigationOptions = {
+  title: "Home Screen"
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const React_Stack_Navigation = StackNavigator({
+  Home: { screen: reactNavigationSample },
+  SecondScreen: { screen: SecondScreen, title: "Second Screen" }
 });
+
 
 AppRegistry.registerComponent('React_Stack_Navigation', () => React_Stack_Navigation);
